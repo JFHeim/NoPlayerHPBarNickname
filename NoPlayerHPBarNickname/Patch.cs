9885db6ea9502b1bad58d71478f8d7f32ed568cb
Vyacheslav <imjustafrogger@gmail.com>
2025-09-29 19:38:59 +0300

    Fixes and refactor

--- NoPlayerHPBarNickname/Patch.cs
+++ NoPlayerHPBarNickname/Patch.cs
@@ -8,8 +8,8 @@ public static class Patch
     [HarmonyPatch(typeof(EnemyHud), nameof(EnemyHud.LateUpdate))] [HarmonyPostfix] [HarmonyWrapSafe]
     private static void UpdateHuds(EnemyHud __instance)
     {
-        if (!__instance || __instance.m_huds == null || __instance.m_huds.Count <= 0 || !m_localPlayer) return;
-        var maxShowDistance = anyHudMaxShowDistance.Value;
+        if (__instance == null || __instance.m_huds == null || __instance.m_huds.Count <= 0 || m_localPlayer == null) return;
+        var maxShowDistance = AnyHudMaxShowDistanceConfig.Value;
         if (maxShowDistance != 0) __instance.m_maxShowDistance = maxShowDistance;
 
         foreach (var hud in __instance.m_huds)
@@ -17,29 +17,35 @@ public static class Patch
             var character = hud.Key;
             var data = hud.Value;
             if (character == null || data == null || character.IsDead()) continue;
+            
             var nickObj = data.m_name?.transform.gameObject;
+            if (!nickObj) continue;
+            
             var healthObj = data.m_gui?.transform.Find("Health")?.gameObject;
-            if (!nickObj || !healthObj) continue;
+            if (!healthObj) continue;
+            
             var distance = Utils.DistanceXZ(m_localPlayer.transform.position, character.transform.position);
             int nickDistance, healthDistance;
             if (character.IsPlayer())
             {
-                nickDistance = players_nameDistance.Value;
-                healthDistance = players_barDistance.Value;
+                nickDistance = PlayersNameDistanceConfig.Value;
+                healthDistance = PlayersBarDistanceConfig.Value;
             } else
             {
-                nickDistance = mobs_nameDistance.Value;
-                healthDistance = mobs_barDistance.Value;
-                var guiTransform = data.m_gui.transform;
-                var Alerted = guiTransform.Find("Alerted").gameObject;
-                Alerted.SetActive(mobs_alertedSignDistance.Value != 0
-                                  && distance < mobs_alertedSignDistance.Value);
+                nickDistance = MobsNameDistanceConfig.Value;
+                healthDistance = MobsBarDistanceConfig.Value;
+                var guiTransform = data.m_gui?.transform;
+                var alerted = guiTransform?.Find("Alerted")?.gameObject;
+                alerted?.SetActive(MobsAlertedSignDistanceConfig.Value != 0 && distance < MobsAlertedSignDistanceConfig.Value);
 
-                var showStars = mobs_starsDistance.Value != 0 && distance < mobs_starsDistance.Value;
-                var level_2 = guiTransform.Find("level_2").gameObject;
-                var level_3 = guiTransform.Find("level_3").gameObject;
-                level_2.SetActive(showStars);
-                level_3.SetActive(showStars);
+                var showStars = MobsStarsDistanceConfig.Value != 0 && distance < MobsStarsDistanceConfig.Value;
+                if (guiTransform != null)
+                {
+                    var level_2 = guiTransform.Find("level_2").gameObject;
+                    var level_3 = guiTransform.Find("level_3").gameObject;
+                    level_2.SetActive(showStars);
+                    level_3.SetActive(showStars);
+                }
             }
 
             nickObj.SetActive(nickDistance != 0 && distance < nickDistance);
--- NoPlayerHPBarNickname/Plugin.cs
+++ NoPlayerHPBarNickname/Plugin.cs
@@ -1,48 +1,39 @@
-﻿using BepInEx;
+﻿using System.Reflection;
+using BepInEx;
 using BepInEx.Configuration;
 
 namespace NoPlayerHPBarNickname;
 
-[BepInPlugin(ModGUID, ModName, ModVersion)]
+[BepInPlugin(ModGuid, ModName, ModVersion)]
 public class Plugin : BaseUnityPlugin
 {
     private const string
         ModName = "NoPlayerHPBarNickname",
-        ModVersion = "1.5.2",
+        ModVersion = "1.5.4",
         ModAuthor = "Frogger",
-        ModGUID = $"com.{ModAuthor}.{ModName}";
+        ModGuid = $"com.{ModAuthor}.{ModName}";
 
-    public static ConfigEntry<int> mobs_nameDistance;
-    public static ConfigEntry<int> mobs_barDistance;
-    public static ConfigEntry<int> mobs_alertedSignDistance;
-    public static ConfigEntry<int> mobs_awareSignDistance;
-    public static ConfigEntry<int> mobs_starsDistance;
-    public static ConfigEntry<int> players_nameDistance;
-    public static ConfigEntry<int> players_barDistance;
-    public static ConfigEntry<int> anyHudMaxShowDistance;
+    public static ConfigEntry<int> MobsNameDistanceConfig         = null!;
+    public static ConfigEntry<int> MobsBarDistanceConfig          = null!;
+    public static ConfigEntry<int> MobsAlertedSignDistanceConfig  = null!;
+    public static ConfigEntry<int> MobsStarsDistanceConfig        = null!;
+    public static ConfigEntry<int> PlayersNameDistanceConfig      = null!;
+    public static ConfigEntry<int> PlayersBarDistanceConfig       = null!;
+    public static ConfigEntry<int> AnyHudMaxShowDistanceConfig    = null!;
 
 
     private void Awake()
     {
-        CreateMod(this, ModName, ModAuthor, ModVersion, ModGUID);
+        CreateMod(this, ModName, ModAuthor, ModVersion, ModGuid);
 
-        mobs_barDistance = config("Mobs", "Mobs healthBar distance", 6,
-            "If mob is more than this distance from player, its health bar will be hidden. Set to 0 to hide health bar completely");
-        mobs_nameDistance = config("Mobs", "Mobs name distance", 2,
-            "If mob is more than this distance from player, its name will be hidden. Set to 0 to hide name completely");
-        mobs_alertedSignDistance = config("Mobs", "Mobs alert sign distance", 5,
-            "If mob is more than this distance from player, its alert sign will be hidden. Set to 0 to hide alert sign completely");
-        mobs_starsDistance = config("Mobs", "Mobs stars distance", 5,
-            "If mob is more than this distance from player, its stars will be hidden. Set to 0 to hide stars completely");
-        mobs_awareSignDistance = config("Mobs", "Mobs aware sign distance", 5,
-            "If mob is more than this distance from player, its aware sign will be hidden. Set to 0 to hide aware sign completely");
+        MobsBarDistanceConfig            = config("Mobs",    "Mobs healthBar distance",      6, "If mob is more than this distance from player, its health bar will be hidden. Set to 0 to hide health bar completely");
+        MobsNameDistanceConfig           = config("Mobs",    "Mobs name distance",           2, "If mob is more than this distance from player, its name will be hidden. Set to 0 to hide name completely");
+        MobsAlertedSignDistanceConfig    = config("Mobs",    "Mobs alert sign distance",     5, "If mob is more than this distance from player, its alert sign will be hidden. Set to 0 to hide alert sign completely");
+        MobsStarsDistanceConfig          = config("Mobs",    "Mobs stars distance",          5, "If mob is more than this distance from player, its stars will be hidden. Set to 0 to hide stars completely");
 
-        players_barDistance = config("Players", "Players healthBar distance", 6,
-            "If player is more than this distance from player, his/her health bar will be hidden. Set to 0 to hide health bar completely");
-        players_nameDistance = config("Players", "Players name distance", 2,
-            "If player is more than this distance from player, his/her name will be hidden. Set to 0 to hide name completely");
-
-        anyHudMaxShowDistance = config("Other", "Any hud max show distance. Warning: Read description", 0,
-            "This overrides vanilla hud max show distance. Set to 0 to disable and keep vanilla value. Vanilla value is 30.");
+        PlayersBarDistanceConfig         = config("Players", "Players healthBar distance",   6, "If player is more than this distance from player, his/her health bar will be hidden. Set to 0 to hide health bar completely");
+        PlayersNameDistanceConfig        = config("Players", "Players name distance",        2, "If player is more than this distance from player, his/her name will be hidden. Set to 0 to hide name completely");
+    
+        AnyHudMaxShowDistanceConfig      = config("Other",   "Any hud max show distance. Warning: Read description", 0, "This overrides vanilla hud max show distance. Set to 0 to disable and keep vanilla value. Vanilla value is 30.");
     }
 }
